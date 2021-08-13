13976eda0ac74a34fbb5c8846c507f0c32bc734a
samilait <sami.p.laitinen@gmail.com>
2021-08-13 22:25:06 +0300

    add utils

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 const http = require('http')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -41,6 +43,6 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
